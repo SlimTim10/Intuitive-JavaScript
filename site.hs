a4f8b4f98bced84d9b5323c63910300303fba6a3
SlimTim10 <slimtim10@gmail.com>
2021-12-14 18:20:21 -0500

    Table of contents in lessons

--- site.hs
+++ site.hs
@@ -2,11 +2,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.Monoid ((<>))
 import           Hakyll
+import qualified Text.Pandoc as Pandoc
 import qualified GHC.IO.Encoding as E
 import Control.Applicative (empty)
 import Data.Maybe (fromMaybe)
 import Data.List (isPrefixOf)
-
+import qualified Data.Text as T
 
 --------------------------------------------------------------------------------
 main :: IO ()
@@ -32,9 +33,15 @@ main = do
     match (fromList ["preface.org"]) $ do
       route $ (gsubRoute "preface.org" (const "index")) `composeRoutes` (setExtension "html")
       compile $ do
+        lessons <- loadAll "lessons/*.org"
+        let tocContext =
+              field "lessonNumber" lessonNumber <>
+              defaultContext
         let prefaceContext =
+              listField "toc" tocContext (return lessons) <>
               field "firstLessonUrl" firstLessonUrl <>
               field "firstLessonTitle" firstLessonTitle <>
+              constField "preface" "true" <>
               defaultContext
         pandocCompiler
           >>= loadAndApplyTemplate "templates/lesson.html" prefaceContext
@@ -45,11 +52,11 @@ main = do
       route $ setExtension "html"
       compile $ do
         let lessonContext =
+              field "lessonNumber" lessonNumber <>
               field "nextLessonUrl" nextLessonUrl <>
               field "nextLessonTitle" nextLessonTitle <>
-              field "lessonNumber" lessonNumber <>
               defaultContext
-        pandocCompiler
+        pandocCompilerWith defaultHakyllReaderOptions withTOC
           >>= loadAndApplyTemplate "templates/lesson.html"    lessonContext
           >>= loadAndApplyTemplate "templates/default.html" lessonContext
           >>= relativizeUrls
@@ -61,6 +68,26 @@ config :: Configuration
 config = defaultConfiguration
   { destinationDirectory = "docs"
   }
+
+withTOC :: Pandoc.WriterOptions
+withTOC = defaultHakyllWriterOptions
+  { Pandoc.writerNumberSections = False
+  , Pandoc.writerTableOfContents = True
+  , Pandoc.writerTOCDepth = 2
+  , Pandoc.writerTemplate = Just tocTemplate
+  }
+  where
+    tocTemplate =
+      either error id $ either (error . show) id
+      $ Pandoc.runPure $ Pandoc.runWithDefaultPartials
+      $ Pandoc.compileTemplate ""
+      $ T.unlines
+      [ "<nav id=\"toc\" class=\"card\">"
+      , "<h1>Contents</h1>"
+      , "$toc$"
+      , "</nav>"
+      , "<article class=\"card\"><section id=\"lesson-body\">$body$</section>"
+      ]
 
 firstLessonUrl :: Item String -> Compiler String
 firstLessonUrl _ = do
@@ -76,9 +103,12 @@ firstLessonTitle _ = do
 lessonNumber :: Item String -> Compiler String
 lessonNumber lesson = do
   let path = toFilePath (itemIdentifier lesson)
-  if "lessons/" `isPrefixOf` path
-    then return . take 2 . drop (length ("lessons/" :: String)) $ path
+  -- TODO: use System.FilePath.Posix functions
+  if "lessons" `isPrefixOf` path
+    then return . numPrefix . drop (length ("lessons" :: String) + 1) $ path
     else empty
+  where
+    numPrefix = dropWhile (== '0') . take 2
 
 nextLessonUrl :: Item String -> Compiler String
 nextLessonUrl lesson = do
@@ -94,8 +124,8 @@ nextLessonTitle lesson = do
   let ident = itemAfter lessons $ itemIdentifier lesson
   case ident of
     Just i -> do
-      metadata <- getMetadata i
-      return $ fromMaybe "No title" $ lookupString "title" metadata
+      getMetadataField i "title"
+      >>= return . fromMaybe "No title"
     Nothing -> empty
 
 itemAfter :: Eq a => [a] -> a -> Maybe a
