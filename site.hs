4c642f4911866807261e861450e80cc1ea40ea97
SlimTim10 <slimtim10@gmail.com>
2021-12-14 18:48:40 -0500

    Previous and next lesson links

--- site.hs
+++ site.hs
@@ -55,6 +55,8 @@ main = do
               field "lessonNumber" lessonNumber <>
               field "nextLessonUrl" nextLessonUrl <>
               field "nextLessonTitle" nextLessonTitle <>
+              field "prevLessonUrl" prevLessonUrl <>
+              field "prevLessonTitle" prevLessonTitle <>
               defaultContext
         pandocCompilerWith defaultHakyllReaderOptions withTOC
           >>= loadAndApplyTemplate "templates/lesson.html"    lessonContext
@@ -112,21 +114,51 @@ lessonNumber lesson = do
 
 nextLessonUrl :: Item String -> Compiler String
 nextLessonUrl lesson = do
-  lessons <- getMatches "lessons/*.org"
-  let ident = itemAfter lessons $ itemIdentifier lesson
+  ident <- nextLessonIdentifier lesson
   case ident of
     Just i -> (fmap (maybe empty toUrl) . getRoute) i
     Nothing -> empty
 
 nextLessonTitle :: Item String -> Compiler String
 nextLessonTitle lesson = do
-  lessons <- getMatches "lessons/*.org"
-  let ident = itemAfter lessons $ itemIdentifier lesson
+  ident <- nextLessonIdentifier lesson
   case ident of
     Just i -> do
       getMetadataField i "title"
       >>= return . fromMaybe "No title"
     Nothing -> empty
 
-itemAfter :: Eq a => [a] -> a -> Maybe a
-itemAfter xs x = lookup x $ zip xs (tail xs)
+prevLessonUrl :: Item String -> Compiler String
+prevLessonUrl lesson = do
+  ident <- prevLessonIdentifier lesson
+  case ident of
+    Just i -> (fmap (maybe empty toUrl) . getRoute) i
+    Nothing -> empty
+
+prevLessonTitle :: Item String -> Compiler String
+prevLessonTitle lesson = do
+  ident <- prevLessonIdentifier lesson
+  case ident of
+    Just i -> do
+      getMetadataField i "title"
+      >>= return . fromMaybe "No title"
+    Nothing -> empty
+
+getLessons :: MonadMetadata m => m [Identifier]
+getLessons = getMatches "lessons/*.org"
+
+nextLessonIdentifier :: MonadMetadata m => Item String -> m (Maybe Identifier)
+nextLessonIdentifier lesson = do
+  lessons <- getLessons
+  return $ itemAfter lessons $ itemIdentifier lesson
+  where
+    itemAfter :: Eq a => [a] -> a -> Maybe a
+    itemAfter xs x = lookup x $ zip xs (tail xs)
+
+prevLessonIdentifier :: MonadMetadata m => Item String -> m (Maybe Identifier)
+prevLessonIdentifier lesson = do
+  lessons <- getLessons
+  return $ itemBefore lessons $ itemIdentifier lesson
+  where
+    itemBefore :: Eq a => [a] -> a -> Maybe a
+    itemBefore xs x = lookup x $ zip (tail xs) xs
