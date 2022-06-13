5b7c26716c8b82cf93806c1982e4102384f9ad41
SlimTim10 <slimtim10@gmail.com>
2022-06-13 13:15:32 -0400

    Update numbering system for lesson 4 exercises

--- src/exercises/04-conditionals-exercises.js
+++ src/exercises/04-conditionals-exercises.js
@@ -5,48 +5,48 @@ const num = 12;
 Ignore any lines that look like the following.
 They are here so you can see the exercise numbers in the output.
 */
-process.stdout.write('1. ');
+process.stdout.write('*4·1. ');
 
 /*
-1. Only print the following message if num is greater than 10.
+*4·1. Only print the following message if num is greater than 10.
 Try changing the value of num to make sure your code works.
 */
 console.log('num is greater than 10');
 
 
-process.stdout.write('\n2. ');
+process.stdout.write('\n*4·11. ');
 /*
-2. Only print the message 'num is less than 100' when appropriate.
+*4·11. Only print the message 'num is less than 100' when appropriate.
 */
 
 
-process.stdout.write('\n3. ');
+process.stdout.write('\n*4·12. ');
 /*
-3. Now do the same with 'num is a positive number'.
+*4·12. Now do the same with 'num is a positive number'.
 */
 
 
-process.stdout.write('\n4. ');
+process.stdout.write('\n*4·13. ');
 /*
-4. 'num is between 10 and 100' (exclusive)
+*4·13. 'num is between 10 and 100' (exclusive)
 */
 
 
-process.stdout.write('\n5. ');
+process.stdout.write('\n*4·14. ');
 /*
-5. 'num is even'
+*4·14. 'num is even'
 */
 
 
-process.stdout.write('\n6. ');
+process.stdout.write('\n*4·15. ');
 /*
-6. 'num is an even number between 20 and 30' (inclusive)
+*4·15. 'num is an even number between 20 and 30' (inclusive)
 */
 
 
-process.stdout.write('\n7. ');
+process.stdout.write('\n*4·16. ');
 /*
-7. 'num is odd or negative'
+*4·16. 'num is odd or negative'
 */
 
 
@@ -54,26 +54,26 @@ process.stdout.write('\n7. ');
 const x = 3;
 const y = 5;
 
-process.stdout.write('\n8. ');
+process.stdout.write('\n*4·2. ');
 process.stdout.write('The bigger number is: ');
 /*
-8. Print either x or y, whichever is bigger.
+*4·2. Print either x or y, whichever is bigger.
 */
 
 
-process.stdout.write('\n9. ');
+process.stdout.write('\n*4·21. ');
 /*
-9. Print either the message 'x and y are equal' or 'x and y are not equal'.
+*4·21. Print either the message 'x and y are equal' or 'x and y are not equal'.
 */
 
 
 // Let's add another number into the mix.
 const z = 2;
 
-process.stdout.write('\n10. ');
+process.stdout.write('\n*4·3. ');
 process.stdout.write('The biggest number is: ');
 /*
-10. Print either x, y, or z, whichever is bigger.
+*4·3. Print either x, y, or z, whichever is bigger.
 Remember to change the values of x, y, and z to make sure it works!
 */
 
@@ -82,17 +82,17 @@ Remember to change the values of x, y, and z to make sure it works!
 const walletInCents = 25;
 const priceInDollars = 1.25;
 
-process.stdout.write('\n11. ');
+process.stdout.write('\n*4·4. ');
 /*
-11. Print 'Exact amount!' when the wallet contains the exact amount for the price. Print 'More than enough.' when there is more than enough money in the wallet. Print 'Not enough money.' when there is not enough money in the wallet.
+*4·4. Print 'Exact amount!' when the wallet contains the exact amount for the price. Print 'More than enough.' when there is more than enough money in the wallet. Print 'Not enough money.' when there is not enough money in the wallet.
 
 Notice that the wallet is in cents and the price is in dollars, so you'll have to do a bit of work to compare them.
 */
 
 
-process.stdout.write('\n12. ');
+process.stdout.write('\n*4·41. ');
 /*
-12. Do the same thing as in 11, but let's add in some error handling. In the case where the wallet is negative, print 'Wallet cannot be negative.' When the price is negative, print 'Price cannot be negative.' Think about the importance of the order of the conditions.
+*4·41. Do the same thing as in 11, but let's add in some error handling. In the case where the wallet is negative, print 'Wallet cannot be negative.' When the price is negative, print 'Price cannot be negative.' Think about the importance of the order of the conditions.
 */
 
 
@@ -102,9 +102,9 @@ let password = '1234';
 const expectedUsername = 'Zed';
 const expectedPassword = '9876';
 
-process.stdout.write('\n13. ');
+process.stdout.write('\n*4·5. ');
 /*
-13. To simulate authentication, we have a username, a password, and expected values for each. The login is successful if the username and password match the expected values. Otherwise, either the username is wrong or the password is wrong. Print appropriate messages for each of the 3 cases. (Don't worry about the case where the username and password are both wrong; one of the other cases will catch it.)
+*4·5. To simulate authentication, we have a username, a password, and expected values for each. The login is successful if the username and password match the expected values. Otherwise, either the username is wrong or the password is wrong. Print appropriate messages for each of the 3 cases. (Don't worry about the case where the username and password are both wrong; one of the other cases will catch it.)
 */
 
 
@@ -112,9 +112,9 @@ process.stdout.write('\n13. ');
 const a = 1;
 const b = 2;
 
-process.stdout.write('\n14. ');
+process.stdout.write('\n*4·6. ');
 /*
-14. Print the appropriate message for any of the following cases comparing a and b:
+*4·6. Print the appropriate message for any of the following cases comparing a and b:
 
 Matching positive numbers.
 Matching negative numbers.
@@ -141,9 +141,11 @@ const seniorSalaryMinimum = 95000;
 const seniorSalaryMaximum = 120000;
 const ceoSalaryMinimum = 110000;
 
-process.stdout.write('\n15. ');
+process.stdout.write('\n*4·61. ');
 /*
-15. We have another authentication, but this time with a salary as well. We want to print a personalized welcome message to users who log in. For example, if the username and password match the employee username and password and their salary is within the junior range, print 'Welcome, junior employee Zarya.' In the case where the salary is not within any of the set ranges, simply print 'Welcome, employee Zarya.' If the username and password match the CEO credentials but the salary is below the CEO's minimum salary, we have an imposter! For an invalid login, just print 'Invalid login.' (don't worry about whether it was the username or password that didn't match).
+*4·61. We have another authentication, but this time with a salary as well. We want to print a personalized welcome message to users who log in.
+
+For example, if the username and password match the employee username and password and their salary is within the junior range, print 'Welcome, junior employee Zarya.' In the case where the salary is not within any of the set ranges, simply print 'Welcome, employee Zarya.' If the username and password match the CEO credentials but the salary is below the CEO's minimum salary, we have an imposter! For an invalid login, just print 'Invalid login.' (don't worry about whether it was the username or password that didn't match).
 
 If you notice you're repeating a certain condition more than once, try nesting if statements to avoid the repeitition.
 */
@@ -152,9 +154,9 @@ If you notice you're repeating a certain condition more than once, try nesting i
 // Practice complex conditions
 const creditCard = 123;
 
-process.stdout.write('\n16. ');
+process.stdout.write('\n*4·7. ');
 /*
-16. Print a message saying whether the credit card number is valid or not. A valid credit card in this system must have all of the following properties:
+*4·7. Print a message saying whether the credit card number is valid or not. A valid credit card in this system must have all of the following properties:
 
 - It is exactly 5 digits and doesn't begin with any 0's
 - Tripling it gives an even number
--- src/exercises/04-conditionals-solutions.js
+++ src/exercises/04-conditionals-solutions.js
@@ -5,10 +5,10 @@ const num = 12;
 Ignore any lines that look like the following.
 They are here so you can see the exercise numbers in the output.
 */
-process.stdout.write('1. ');
+process.stdout.write('*4·1. ');
 
 /*
-1. Only print the following message if num is greater than 10.
+*4·1. Only print the following message if num is greater than 10.
 Try changing the value of num to make sure your code works.
 */
 if (num > 10) {
@@ -16,54 +16,54 @@ if (num > 10) {
 }
 
 
-process.stdout.write('\n2. ');
+process.stdout.write('\n*4·11. ');
 /*
-2. Only print the message 'num is less than 100' when appropriate.
+*4·11. Only print the message 'num is less than 100' when appropriate.
 */
 if (num < 100) {
   console.log('num is less than 100');
 }
 
 
-process.stdout.write('\n3. ');
+process.stdout.write('\n*4·12. ');
 /*
-3. Now do the same with 'num is a positive number'.
+*4·12. Now do the same with 'num is a positive number'.
 */
 if (num > 0) {
   console.log('num is a positive number')
 }
 
 
-process.stdout.write('\n4. ');
+process.stdout.write('\n*4·13. ');
 /*
-4. 'num is between 10 and 100' (exclusive)
+*4·13. 'num is between 10 and 100' (exclusive)
 */
 if (num > 10 && num < 100) {
   console.log('num is between 10 and 100');
 }
 
 
-process.stdout.write('\n5. ');
+process.stdout.write('\n*4·14. ');
 /*
-5. 'num is even'
+*4·14. 'num is even'
 */
 if (num % 2 === 0) {
   console.log('num is even');
 }
 
 
-process.stdout.write('\n6. ');
+process.stdout.write('\n*4·15. ');
 /*
-6. 'num is an even number between 20 and 30' (inclusive)
+*4·15. 'num is an even number between 20 and 30' (inclusive)
 */
 if (num % 2 === 0 && num >= 20 && num <= 30) {
   console.log('num is an even number between 20 and 30');
 }
 
 
-process.stdout.write('\n7. ');
+process.stdout.write('\n*4·16. ');
 /*
-7. 'num is odd or negative'
+*4·16. 'num is odd or negative'
 */
 if (num % 2 !== 0 || num < 0) {
   console.log('num is odd or negative');
@@ -74,10 +74,10 @@ if (num % 2 !== 0 || num < 0) {
 const x = 3;
 const y = 5;
 
-process.stdout.write('\n8. ');
+process.stdout.write('\n*4·2. ');
 process.stdout.write('The bigger number is: ');
 /*
-8. Print either x or y, whichever is bigger.
+*4·2. Print either x or y, whichever is bigger.
 */
 if (x > y) {
   console.log(x);
@@ -86,9 +86,9 @@ if (x > y) {
 }
 
 
-process.stdout.write('\n9. ');
+process.stdout.write('\n*4·21. ');
 /*
-9. Print either message 'x and y are equal' or 'x and y are not equal'.
+*4·21. Print either the message 'x and y are equal' or 'x and y are not equal'.
 */
 if (x === y) {
   console.log('x and y are equal');
@@ -100,10 +100,10 @@ if (x === y) {
 // Let's add another number into the mix.
 const z = 2;
 
-process.stdout.write('\n10. ');
+process.stdout.write('\n*4·3. ');
 process.stdout.write('The biggest number is: ');
 /*
-10. Print either x, y, or z, whichever is bigger.
+*4·3. Print either x, y, or z, whichever is bigger.
 Remember to change the values of x, y, and z to make sure it works!
 */
 if (x > y && x > z) {
@@ -119,9 +119,9 @@ if (x > y && x > z) {
 const walletInCents = 25;
 const priceInDollars = 1.25;
 
-process.stdout.write('\n11. ');
+process.stdout.write('\n*4·4. ');
 /*
-11. Print 'Exact amount!' when the wallet contains the exact amount for the price. Print 'More than enough.' when there is more than enough money in the wallet. Print 'Not enough money.' when there is not enough money in the wallet.
+*4·4. Print 'Exact amount!' when the wallet contains the exact amount for the price. Print 'More than enough.' when there is more than enough money in the wallet. Print 'Not enough money.' when there is not enough money in the wallet.
 
 Notice that the wallet is in cents and the price is in dollars, so you'll have to do a bit of work to compare them.
 */
@@ -134,9 +134,9 @@ if (walletInCents === priceInDollars * 100) {
 }
 
 
-process.stdout.write('\n12. ');
+process.stdout.write('\n*4·41. ');
 /*
-12. Do the same thing as in 11, but let's add in some error handling. In the case where the wallet is negative, print 'Wallet cannot be negative.' When the price is negative, print 'Price cannot be negative.' Think about the importance of the order of the conditions.
+*4·41. Do the same thing as in 11, but let's add in some error handling. In the case where the wallet is negative, print 'Wallet cannot be negative.' When the price is negative, print 'Price cannot be negative.' Think about the importance of the order of the conditions.
 */
 if (walletInCents < 0) {
   console.log('Wallet cannot be negative.');
@@ -157,9 +157,9 @@ let password = '1234';
 const expectedUsername = 'Zed';
 const expectedPassword = '9876';
 
-process.stdout.write('\n13. ');
+process.stdout.write('\n*4·5. ');
 /*
-13. To simulate authentication, we have a username, a password, and expected values for each. The login is successful if the username and password match the expected values. Otherwise, either the username is wrong or the password is wrong. Print appropriate messages for each of the 3 cases. (Don't worry about the case where the username and password are both wrong; one of the other cases will catch it.)
+*4·5. To simulate authentication, we have a username, a password, and expected values for each. The login is successful if the username and password match the expected values. Otherwise, either the username is wrong or the password is wrong. Print appropriate messages for each of the 3 cases. (Don't worry about the case where the username and password are both wrong; one of the other cases will catch it.)
 */
 if (username === expectedUsername && password === expectedPassword) {
   console.log('Login successful!');
@@ -174,9 +174,9 @@ if (username === expectedUsername && password === expectedPassword) {
 const a = 1;
 const b = 2;
 
-process.stdout.write('\n14. ');
+process.stdout.write('\n*4·6. ');
 /*
-14. Print the appropriate message for any of the following cases comparing a and b:
+*4·6. Print the appropriate message for any of the following cases comparing a and b:
 
 Matching positive numbers.
 Matching negative numbers.
@@ -218,14 +218,11 @@ const seniorSalaryMinimum = 95000;
 const seniorSalaryMaximum = 120000;
 const ceoSalaryMinimum = 110000;
 
-process.stdout.write('\n15. ');
+process.stdout.write('\n*4·61. ');
 /*
-15. We have another authentication, but this time with a salary as well. We want to print a personalized welcome message to users who log in.
+*4·61. We have another authentication, but this time with a salary as well. We want to print a personalized welcome message to users who log in.
 
-For example, if the username and password match the employee username and password and their salary is within the junior range, print 'Welcome, junior employee Zarya.'
-In the case where the salary is not within any of the set ranges, simply print 'Welcome, employee Zarya.'
-If the username and password match the CEO credentials but the salary is below the CEO's minimum salary, we have an imposter!
-For an invalid login, just print 'Invalid login.' (don't worry about whether it was the username or password that didn't match).
+For example, if the username and password match the employee username and password and their salary is within the junior range, print 'Welcome, junior employee Zarya.' In the case where the salary is not within any of the set ranges, simply print 'Welcome, employee Zarya.' If the username and password match the CEO credentials but the salary is below the CEO's minimum salary, we have an imposter! For an invalid login, just print 'Invalid login.' (don't worry about whether it was the username or password that didn't match).
 
 If you notice you're repeating a certain condition more than once, try nesting if statements to avoid the repeitition.
 */
@@ -253,9 +250,9 @@ if (myUsername === employeeUsername && myPassword === employeePassword) {
 // Practice complex conditions
 const creditCard = 123;
 
-process.stdout.write('\n16. ');
+process.stdout.write('\n*4·7. ');
 /*
-16. Print a message saying whether the credit card number is valid or not. A valid credit card in this system must have all of the following properties:
+*4·7. Print a message saying whether the credit card number is valid or not. A valid credit card in this system must have all of the following properties:
 
 - It is exactly 5 digits and doesn't begin with any 0's
 - Tripling it gives an even number
